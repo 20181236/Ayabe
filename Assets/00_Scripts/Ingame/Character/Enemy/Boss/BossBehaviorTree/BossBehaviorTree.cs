a78838a6183eedc0c691551a94084abaf6fab6a0
20181236 <70249125+20181236@users.noreply.github.com>
2025-09-17 16:49:20 +0900

    Add BossBehaviorTree
    request BossBehaviorTree

---
+++ Assets/00_Scripts/Ingame/Character/Enemy/Boss/BossBehaviorTree/BossBehaviorTree.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorTree : BehaviorTreeBase
+{
+    public override void BuildBehaviorTree()
+    {
+        // 이 안에서 실제 행동 트리를 구성하는 코드를 작성합니다.
+        // 예: rootNode = new SelectorNode(...);
+        //     rootNode.AddChild(new SequenceNode(...));
+    }
+
+}
